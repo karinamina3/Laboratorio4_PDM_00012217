157ddda722d62a7f1acb3549fc64f444a8d795ce
karinamina3 <0012217@uca.edu.sv>
2019-04-05 15:42:17 -0600

    Laboratorio 4

---
+++ app/src/main/java/com/kmina/labo4/pojos/Movie.kt
@@ -0,0 +1,17 @@
+package com.kmina.labo4.pojos
+
+import java.time.Year
+
+data class Movie (
+    val Titler:String = "N/A",
+    val Year:String = "N/A",
+    val Released:String = "N/A",
+    val Runtime:String = "N/A",
+    val Genre:String = "N/A",
+    val Director:String = "N/A",
+    val Actors:String = "N/A",
+    var Plot:String = "N/A",
+    val Language:String = "N/A",
+    val imdbRating:String = "N/A",
+    val Poster:String = "N/A"
+)
